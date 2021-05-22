297f4821de72be7525246034e2c81d9cbd7d3146
Chirag Narasimha Murthy <c.narasimha.murthy@accenture.com>
2021-05-22 21:35:44 +1000

    more print statements added

---
+++ basics/operators.py
@@ -0,0 +1,11 @@
+a = 12
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)  # integer division, rounded down towards minus infinity
+print(a % b)
+
+for i in range(0, 4):
+    print("i = ", i)
--- basics/strings.py
+++ basics/strings.py
@@ -41,5 +41,59 @@ age = 24
 print(name + " is ", age, "years old")
 print(type(age))
 
-name = input("Please enter your name: ")
-print("Your name is ", name)
+# number = "922,223;372:036 854,775;807"
+number = "012,345;678:901"
+print(number[3::4])
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+backwards = letters[::-1]
+forwards = letters[::1]
+print(backwards)
+print(forwards)
+
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
+      .format(31, "Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"))
+
+print("Jan: {2}, Feb: {0}, Mar: {2}, Apr: {1}, May: {2}, Jun: {1}, Jul: {2}, Sep: {1}, Oct: {2}, Nov: {1}, Dec: {2}"
+      .format(28, 30, 31))
+
+print("""Jan: {2}
+Feb: {0}
+Mar: {2}
+Apr: {1}
+May: {2}
+Jun: {1}
+Jul: {2}
+Sep: {1}
+Oct: {2}
+Nov: {1}
+Dec: {2}"""
+      .format(28, 30, 31))
+
+for i in range(1, 13):
+    print("No. {0:2} squared is {1:3} and cubed is {2:4}".format(i, i ** 2, i ** 3))
+
+print()
+
+for i in range(1, 13):
+    print("No. {0:2} squared is {1:<3} and cubed is {2:^4}".format(i, i ** 2, i ** 3))
+
+print()
+
+print("Pi is approximately {0:12}".format(22 / 7))
+print("Pi is approximately {0:12f}".format(22 / 7))
+print("Pi is approximately {0:12.50f}".format(22 / 7))
+print("Pi is approximately {0:52.50f}".format(22 / 7))
+print("Pi is approximately {0:62.50f}".format(22 / 7))
+print("Pi is approximately {0:<72.50f}".format(22 / 7))
+print("Pi is approximately {0:<72.54f}".format(22 / 7))
+
+for i in range(1, 13):
+    print("No. {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
+
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
+
+# name = input("Please enter your name: ")
+# print("Your name is ", name)
